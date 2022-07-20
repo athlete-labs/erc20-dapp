9f54d2bc6dbbfd58354d13f9004788a668cecccf
Andre Amorim <andre.php6@gmail.com>
2022-07-20 12:07:15 -0300

    register worker

--- contracts/AthleteToken.sol
+++ contracts/AthleteToken.sol
@@ -1,6 +1,6 @@
 // contracts/AthleteToken.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
@@ -12,12 +12,25 @@ contract AthleteToken is ERC20, ERC20Permit, ERC20Votes {
     uint public tokenTotalStake;
     uint public difficultyPerBlock;
     uint public athletePool;
+    uint public coustNewOperator = 100;
+    uint public coustNewScout = 200;
+    uint public coustNewLevel = 50;
+
+    Worker[] public workers;
 
     mapping(address => mapping(uint => Stake)) public stakeSubscriptions;
+
+    enum roles { operator, scout }
 
     struct Stake {
         uint amount;
         uint blockId;
+    }
+
+    struct Worker {
+        roles role;
+        address account;
+        uint8 level;
     }
 
     constructor() ERC20("Athlete Token", "ATH") ERC20Permit("Athlete Token") {
@@ -52,6 +65,20 @@ contract AthleteToken is ERC20, ERC20Permit, ERC20Votes {
 
     function balanceStaked(address wallet, uint _blockNumber) public view returns (uint) {
         return stakeSubscriptions[wallet][_blockNumber].amount;
+    }
+
+    function registerWorker(address wallet, uint8 kind) public {
+        require(kind > 0 && kind < 3);
+        if (kind == 1) {
+            _burn(wallet, coustNewOperator);
+            athletePool += coustNewOperator / 2;
+            workers.push(Worker(roles.operator, wallet, 0));
+        }
+        if (kind == 2) {
+            _burn(wallet, coustNewScout);
+            athletePool += coustNewOperator / 2;
+            workers.push(Worker(roles.scout, wallet, 0));
+        }
     }
 
     function _afterTokenTransfer(address from, address to, uint256 amount)
